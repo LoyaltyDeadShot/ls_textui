7fff45d859246be3478e91780b798ef9710ebd5a
ProjectLosSantos <projectlossantoshu@gmail.com>
2025-01-13 12:59:39 +0100

    Added table support for the text ui's, Added Icon support and better width handle

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,11 +2,22 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
+version '1.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua'
+}
+
 client_scripts {
     'client/main.lua'
 }
 
+server_scripts {
+    'server/main.lua'
+}
+
 files {
+    'web/background.png',
     'web/index.html',
     'web/index.js',
 }
--- web/index.js
+++ web/index.js
@@ -1,3 +1,20 @@
+async function fetchNui(event, data = {}) {
+    try {
+        const response = await fetch(`https://${GetParentResourceName()}/${event}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            },
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        return null;
+    }
+}
+
 const App = Vue.createApp({
     data() {
         return {
@@ -6,30 +23,46 @@ const App = Vue.createApp({
         }
     },
     methods: {
-        onMessage(event) {
+        async onMessage(event) {
             if (event.data.type == "showTextUI") {
-                if (typeof event.data.id != 'string' && typeof event.data.key && typeof event.data.message != 'string') return
-                
+                if (typeof event.data.id != 'string' || typeof event.data.message != 'string' || 
+                    (typeof event.data.key != 'string' && typeof event.data.icon != 'string')) return
+
                 const exist = this.textUIs.some(ui => ui.id === event.data.id);
 
                 if(!exist) {
                     this.textUIs.push({
                         id: event.data.id,
-                        key: event.data.key,
-                        message: event.data.message
+                        key: event.data.key || null,
+                        icon: event.data.icon || null,
+                        message: event.data.message,
                     });
-
                 } else {
+                    console.log('Text ui with the id is already exist')
                     return
                 }
             } else if (event.data.type == 'hideTextUI') {
                 this.textUIs = this.textUIs.filter(ui => ui.id !== event.data.id);
             } else if (event.data.type = 'getTextUIs') {
                 const uiIds = this.textUIs.map(ui => ui.id);
-                fetch(`https://${GetParentResourceName()}/returnTextUIs`, {
-                    method: 'POST',
-                    body: JSON.stringify(uiIds)
-                });
+                const timeout = new Promise((resolve) => setTimeout(() => resolve(false), 5000));
+                
+                try {
+                    const result = await Promise.race([
+                        fetchNui('returnTextUIs', uiIds),
+                        timeout
+                    ]);
+                    
+                    if (!result) {
+                        console.log('Failed to get the ids');
+                        return false;
+                    }
+                    
+                    return result;
+                } catch (error) {
+                    console.error('Error occured when fetching the ids');
+                    return false;
+                }
             }
         },
     },
